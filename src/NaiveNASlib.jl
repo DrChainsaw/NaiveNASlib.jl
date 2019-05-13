7f2f9c87fc0009397b362a958ed05a760e0ed770
DrChainsaw <Christian.kyril.skarby@gmail.com>
2019-05-13 20:10:43 +0200

    Files generated by PkgTemplates

---
+++ src/NaiveNASlib.jl
@@ -0,0 +1,5 @@
+module NaiveNASlib
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using NaiveNASlib
+using Test
+
+@testset "NaiveNASlib.jl" begin
+    # Write your own tests here.
+end
