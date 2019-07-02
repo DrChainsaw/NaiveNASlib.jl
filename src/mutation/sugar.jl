ea3db91817922b3174755588550bd6a4d257fd29
DrChainsaw <Christian.kyril.skarby@gmail.com>
2019-07-02 20:03:26 +0200

    Added conc, + and *

--- src/mutation/sugar.jl
+++ src/mutation/sugar.jl
@@ -62,7 +62,7 @@ julia> v([1 2])
 absorbvertex(computation, outsize::Integer, inputs::AbstractVertex...; mutation=IoChange, traitdecoration=identity) = vertex(computation, outsize, traitdecoration(SizeAbsorb()), inputs..., mutation=mutation)
 
 """
-    conc(vs::AbstractVertex...; dims, mutation=IoChange, traitdecoration=identity)
+    conc(v::AbstractVertex, vs::AbstractVertex...; dims, mutation=IoChange, traitdecoration=identity)
 
 Return a mutable vertex which concatenates input along dimension `dim`.
 # Examples
@@ -88,6 +88,122 @@ julia> v([1], [2, 3], [4, 5, 6])
  4
  5
  6
+```
+"""
+conc(v::AbstractVertex, vs::AbstractVertex...; dims, mutation=IoChange, traitdecoration=identity) = vertex((x...) -> cat(x..., dims=dims), nout(v) + sum(nout.(vs)), traitdecoration(SizeStack()), v, vs..., mutation=mutation)
+
+struct VertexConf
+    mutation
+    traitdecoration
+end
+mutationconf(m) = VertexConf(m, identity)
+traitconf(t) = VertexConf(IoChange, t)
+VertexConf() = VertexConf(IoChange, identity)
+
+function invariant_outsize(vs::AbstractVertex...)
+    outsize = unique([nout.(vs)...])
+    length(outsize) == 1 || throw(DimensionMismatch("Dimensions must match! Got $([nout.(vs)...])"))
+    return outsize[]
+end
+
+# Common wiring for all elementwise operations
+elemwise(op, conf::VertexConf, vs::AbstractVertex...) = vertex((x...) -> op.(x...), invariant_outsize(vs...), conf.traitdecoration(SizeInvariant()), vs..., mutation=conf.mutation)
 
 """
-conc(vs::AbstractVertex...; dims, mutation=IoChange, traitdecoration=identity) = vertex((x...) -> cat(x..., dims=dims), sum(nout.(vs)), traitdecoration(SizeStack()), vs..., mutation=mutation)
+    +(v::AbstractVertex, vs::AbstractVertex...)
+    +(conf::VertexConf, v::AbstractVertex, vs::AbstractVertex...)
+
+Return a mutable vertex which performs (broadcasted) elementwise addition of its inputs.
+
+A `VertexConf` with functions to create `MutationState` and `MutationTrait` can be supplied as first argument.
+
+# Examples
+
+```julia-repl
+julia> using NaiveNASlib
+
+julia> v = inputvertex("in1", 2) + inputvertex("in2", 2) + inputvertex("in3" ,2);
+
+julia> nin(v)
+3-element Array{Int64,1}:
+ 2
+ 2
+ 2
+
+julia> nout(v)
+2
+
+julia> v([1, 2], [3, 4], [5, 6])
+2-element Array{Int64,1}:
+  9
+ 12
+
+julia> name(v)
+"MutationVertex::SizeInvariant"
+
+julia> typeof(op(v))
+IoChange
+
+julia> conf = VertexConf(IoSize, t -> NamedTrait(t, "v"));
+
+julia> v = conf + inputvertex("in1", 3) + inputvertex("in2", 3);
+
+julia> name(v)
+"v"
+
+julia> typeof(op(v))
+IoSize
+```
+"""
+Base.:+(conf::VertexConf, v::AbstractVertex, vs::AbstractVertex...) = elemwise(+, conf, v, vs...)
+Base.:+(v::AbstractVertex, vs::AbstractVertex...) = +(VertexConf(), v, vs...)
+
+"""
+    *(v::AbstractVertex, vs::AbstractVertex...)
+    *(conf::VertexConf, v::AbstractVertex, vs::AbstractVertex...)
+
+Return a mutable vertex which performs (broadcasted) elementwise multiplication of its inputs.
+
+A `VertexConf` with functions to create `MutationState` and `MutationTrait` can be supplied as first argument.
+
+# Examples
+
+```julia-repl
+julia> using NaiveNASlib
+
+julia> v = inputvertex("in1", 2) * inputvertex("in2", 2) * inputvertex("in3" ,2);
+
+julia> nin(v)
+3-element Array{Int64,1}:
+ 2
+ 2
+ 2
+
+julia> nout(v)
+2
+
+julia> v([1, 2], [3, 4], [5, 6])
+2-element Array{Int64,1}:
+ 15
+ 48
+
+julia> name(v)
+"MutationVertex::SizeInvariant"
+
+julia> typeof(op(v))
+IoChange
+
+julia> conf = VertexConf(IoSize, t -> NamedTrait(t, "v"));
+
+julia> v = conf * inputvertex("in1", 3) * inputvertex("in2", 3);
+
+julia> name(v)
+"v"
+
+julia> typeof(op(v))
+IoSize
+
+```
+"""
+Base.:*(conf::VertexConf, v::AbstractVertex, vs::AbstractVertex...) = elemwise(*, conf, v, vs...)
+Base.:*(v::AbstractVertex, vs::AbstractVertex...) = *(VertexConf(), v, vs...)
--- test/mutation/sugar.jl
+++ test/mutation/sugar.jl
@@ -22,4 +22,31 @@ import NaiveNASlib
         @test v(1, [2, 3]) == [1,2,3]
         @test name(v) == "v"
     end
+
+    @testset "Create elemwise" begin
+        conf = traitconf(t -> NamedTrait(t, "v"))
+        v = conf + inputvertex("in1", 2) + inputvertex("in2", 2) + inputvertex("in3" ,2)
+
+        @test nin(v) == [2, 2, 2]
+        @test nout(v) == 2
+        @test v([1 2], [3 4], [5 6]) == [9 12]
+        @test v([1, 2], [3, 4], [5, 6]) == [9, 12]
+
+        @test_throws DimensionMismatch inputvertex("in1", 2) + inputvertex("in2", 3)
+
+        v = inputvertex("in1", 2) + inputvertex("in2", 2)
+        @test v([1, 2], [3, 4]) == [4, 6]
+
+        v = conf * inputvertex("in1", 2) * inputvertex("in2", 2) * inputvertex("in3" ,2)
+
+        @test nin(v) == [2, 2, 2]
+        @test nout(v) == 2
+        @test v([1 2], [3 4], [5 6]) == [15 48]
+        @test v([1, 2], [3, 4], [5, 6]) == [15, 48]
+
+        @test_throws DimensionMismatch inputvertex("in1", 2) * inputvertex("in2", 3)
+
+        v = inputvertex("in1", 2) * inputvertex("in2", 2)
+        @test v([1, 2], [3, 4]) == [3, 8]
+    end
 end
