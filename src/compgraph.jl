631900e53ab716c60a7dab76605e16721da1d414
Christian Skärby <christian.skarby@ericsson.com>
2019-07-13 13:56:06 +0200

    Add vertices function

--- src/compgraph.jl
+++ src/compgraph.jl
@@ -128,6 +128,30 @@ function flatten(v::AbstractVertex, vertices::Array{AbstractVertex,1} = Array{Ab
     return vertices
 end
 
+"""
+    vertices(g::CompGraph)
+
+Return an topologically sorted array of all vertices in the graph `g`.
+
+# Examples
+```julia-repl
+julia> ins = InputVertex.(1:2);
+
+julia> v1 = CompVertex(+, ins...);
+
+julia> v2 = CompVertex(*, v1, ins[2]);
+
+julia> graph = CompGraph(ins, v2);
+
+julia> vertices(graph)
+4-element Array{AbstractVertex,1}:
+ InputVertex(2)
+ InputVertex(1)
+ CompVertex(+), inputs=[InputVertex(1), InputVertex(2)]
+ CompVertex(*), inputs=[CompVertex(+), InputVertex(2)]
+```
+"""
+vertices(g::CompGraph) = unique(mapfoldl(flatten, vcat, g.outputs))
 
 """
     copy(g::CompGraph, opfun=cloneop)
--- src/mutation/structure.jl
+++ src/mutation/structure.jl
@@ -552,7 +552,9 @@ end
 """
     insert!(vin::AbstractVertex, factory::Function, outselect::Function=identity)
 
-Replace `vin` as input to all outputs of `vin` with vertex produced by `factory`
+Replace `vin` as input to all outputs of `vin` with vertex produced by `factory`.
+
+The function `factory` takes `vin` as input. 
 
 Example:
 
