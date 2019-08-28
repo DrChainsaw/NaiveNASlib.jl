43c635341552f481af1d89e694de28b7fc8f855d
DrChainsaw <Christian.kyril.skarby@gmail.com>
2019-08-28 20:41:43 +0200

    Add MWE for Appveyor failure

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,36 @@ import JuMP
 include("testutil.jl")
 
 @testset "NaiveNASlib.jl" begin
+
+        @testset "Junipertest" begin
+                import JuMP
+                import Juniper
+                import Ipopt
+                import Cbc
+
+                optimizer = Juniper.Optimizer
+                params = Dict{Symbol,Any}()
+                params[:nl_solver] = JuMP.with_optimizer(Ipopt.Optimizer, print_level=0, sb="yes")
+                params[:mip_solver] = JuMP.with_optimizer(Cbc.Optimizer, logLevel=0)
+                params[:log_levels] = []
+                model = JuMP.Model(JuMP.with_optimizer(optimizer, params))
+
+                x = JuMP.@variable(model, x[1:9], Int)
+                JuMP.@constraint(model, x .>= 1)
+
+                JuMP.@constraint(model, x[2] == 10)
+                JuMP.@constraint(model, x[3] == x[2])
+                JuMP.@constraint(model, x[7] == x[2])
+                JuMP.@constraint(model, x[4] == 3)
+                JuMP.@constraint(model, x[6] + x[8] == x[7])
+
+                xtargets = [9, 8, 8, 3, 3, 5, 8, 3, 4]
+                objective = JuMP.@NLexpression(model, objective[i=1:length(x)], (x[i]/xtargets[i] - 1)^2)
+                JuMP.@NLobjective(model, Min, sum(objective[i] for i in 1:length(objective)))
+
+                JuMP.optimize!(model)
+        end
+
         @info "Testing computation"
 
         include("vertex.jl")
