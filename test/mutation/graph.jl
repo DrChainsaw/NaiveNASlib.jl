0c573c6c308c1f5c0ff2f37fe2b5b786fbea7280
DrChainsaw <christian.kyril.skarby@gmail.com>
2021-07-03 19:12:32 +0200

    Fix tests

--- test/mutation/graph.jl
+++ test/mutation/graph.jl
@@ -5,10 +5,10 @@
     nt(name) = t -> NamedTrait(t, name)
     tf(name) = t -> nt(name)(t)
     iv(size, name="in") = inputvertex(name, size)
-    av(in, outsize, name) = absorbvertex(MatMul(nout(in), outsize), outsize, in, traitdecoration=tf(name))
     tv(in, name) = invariantvertex(identity, in, traitdecoration=tf(name))
+    av(in, outsize, name) = absorbvertex(IndMem(MatMul(nout(in), outsize)), in, traitdecoration=tf(name))
+    cc(ins...; name) = conc(ins...; dims=2, traitdecoration=tf(name), outwrap=x -> IndMem(x, collect(nout.(ins)), sum(nout, ins)))
 
-    cc(ins...; name) = conc(ins...; dims=2, traitdecoration=tf(name))
     nc(name) = traitconf(nt(name))
 
     @testset "SizeDiGraph" begin
@@ -75,15 +75,25 @@
     end
 
     @testset "ΔSizeGraph" begin
-        changed(v) = filter(vi -> nout_org(vi) != nout(vi) || nin(vi) != nin_org(vi), all_in_graph(v))
+        changed(vs) = filter(vi -> trait(vi) !== Immutable() && (ismissing(lastins(vi)) || any(<(1), lastouts(vi)) || any(nins -> any(<(1), nins), lastins(vi))), vs)
 
         function testedge(::Input, src, dst, size)
-            @test nout_org(src) == size
-            @test nin_org(dst)[inputs(dst) .== src] == [size]
+            @test count(>(0), lastouts(src)) == size
+            instocheck = lastins(dst)
+            if instocheck isa Vector{Int}
+                @test count(>(0), instocheck) == size
+            else
+                @test map(ins -> count(>(0), ins), instocheck[inputs(dst) .== src]) == [size]
+            end
         end
         function testedge(::Output, src, dst, size)
-            @test nout_org(dst) == size
-            @test nin_org(src)[inputs(src) .== dst] == [size]
+            @test count(>(0), lastouts(dst)) == size
+            instocheck = lastins(src)
+            if instocheck isa Vector{Int}
+                @test count(>(0), instocheck) == size
+            else
+                @test map(ins -> count(>(0), ins), instocheck[inputs(src) .== dst]) == [size]
+            end
         end
 
         @testset "ΔnoutSizeGraph" begin
@@ -93,13 +103,13 @@
             v3 = cc(v1, v2, name="v3")
             v4 = av(v1, 3, "v4")
             v5 = av(v3, 3, "v5")
-            v6 = nc("v6") >> v4 + v5
+            v6 = VertexConf(nt("v6"), f -> IndMem(f, [nout(v4)], nout(v4))) >> v4 + v5
             v7 = cc(v6, v2, name="v7")
 
             g = ΔnoutSizeGraph(v5)
             Δnout(v5, 2)
 
-            clist = changed(v5)
+            clist = changed(all_in_graph(v0))
             glist = NaiveNASlib.vertexproplist(g, :vertex)
             for v in clist
                 @test v in glist
@@ -108,7 +118,6 @@
             for e in edges(g)
                 testedge(get_prop(g, e, :direction), g[e.src,:vertex], g[e.dst, :vertex], get_prop(g, e, :size))
             end
-
         end
 
         @testset "ΔninSizeGraph" begin
@@ -117,13 +126,13 @@
             v2 = av(v0, 5, "v2")
             v3 = cc(v1, v2, name="v3")
             v4 = av(v0, 9, "v4")
-            v5 = nc("v5") >> v4 + v3
+            v5 = VertexConf(nt("v5"), f -> IndMem(f, [nout(v4)], nout(v4)))  >> v4 + v3
             v6 = av(v5, 3, "v6")
 
             g = ΔninSizeGraph(v3)
             Δnin(v3, 2, 1)
 
-            clist = changed(v3)
+            clist = changed(all_in_graph(v0))
             @test Set(clist) == Set(NaiveNASlib.vertexproplist(g, :vertex))
 
             for e in edges(g)
