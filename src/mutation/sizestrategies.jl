0bc10053e067da337e9227fb9d7802a61382cccc
DrChainsaw <christian.kyril.skarby@gmail.com>
2021-07-03 16:32:04 +0200

    Brush up docstrings

--- src/mutation/sizestrategies.jl
+++ src/mutation/sizestrategies.jl
@@ -85,32 +85,88 @@ struct Relaxed end
 
 """
     ΔNout <: AbstractJuMPΔSizeStrategy
-    ΔNout{T}(vertex::AbstractVertex, Δ::Integer)
-    ΔNoutExact(vertex::AbstractVertex, Δ::Integer, fallback::AbstractJuMPΔSizeStrategy)
-    ΔNoutRelaxed(vertex::AbstractVertex, Δ::Integer, fallback::AbstractJuMPΔSizeStrategy)
+    ΔNout{T, V, F}(Δs::Dict{V, Int}, fallback::F)
 
-Strategy for changing nout of `vertex` by `Δ`, i.e new size is `nout(vertex) + Δ`.
+Strategy for changing nout of vertices.
+    
+For each key-value pair in `v, Δ` in `Δs`, change `nout(v)` by `Δ`, i.e new size is `nout(v) + Δ`.
 
-If `T == Exact`, size change will be added as a constraint to the model which means that the operation will fail if it is not possible to change `nout(vertex)` by exactly `Δ`. If the operation fails, it will be retried with the `fallback` strategy (default `ΔNoutRelaxed`).
+If `T == Exact`, size change will be added as a constraint to the model which means that the operation will fail
+if it is not possible to change `nout(v)` by exactly `Δ`.
 
-If `T == Relaxed`, size change will be added as an objective to the model which means that `nout(vertex)` might not change by exactly `Δ`. In addition, a constraint that `nout(vertex)` must change is also added.
-
-If the operation fails, it will be retried with the `fallback` strategy (default `ΔNout{Relaxed}` if `T==Exact` and `ThrowΔSizeFailError` if `T==Relaxed`).
+If `T == Relaxed`, size change will be added as an objective to the model which means that `nout(v)` might 
+not change by exactly `Δ`. In addition, a constraint that `nout(v)` must change is also added.
 """
 struct ΔNout{T, V, F} <: AbstractJuMPΔSizeStrategy
     Δs::Dict{V, Int}
     fallback::F
 end
-ΔNoutExact(args...; fallback=default_noutfallback(ΔNoutRelaxed,"nout", args)) = ΔNout{Exact}(args...;fallback)
-ΔNoutRelaxed(args...;fallback=default_noutfallback("nout", args)) = ΔNout{Relaxed}(args...;fallback)
+fallback(s::ΔNout) = s.fallback
+
+generic_Δnin_docstring_examples(fname::String; kwargs...) = generic_Δnin_docstring_examples(args ->  "$fname($args)"; kwargs...)
+generic_Δnin_docstring_examples(fgen; footer = "```") = generic_Δnout_docstring_examples(fgen; footer="") * """
+
+# Assume v1 has three inputs and v2 has two inputs instead
+julia> NaiveNASlib.inputs(v::DummyVertex) = v === v1 ? [v,v,v] : [v,v]
+
+julia> $(fgen("v1, 3, missing, 2"));
+
+julia> $(fgen("v1 => (3, missing, 2), v2 => (-2, 0)"));
+""" * footer
+
+generic_Δnout_docstring_examples(fname::String; kwargs...) = generic_Δnout_docstring_examples(args -> "$fname($args)"; kwargs...)
+generic_Δnout_docstring_examples(fgen; footer = "```") = """
+### Examples
+```julia-repl
+julia> struct DummyVertex <: NaiveNASlib.AbstractVertex id::Int end
+
+julia> NaiveNASlib.inputs(v::DummyVertex) = [v]
+
+julia> v1,v2 = DummyVertex(1), DummyVertex(2)
+
+julia> $(fgen("v1, 3"));
+
+julia> $(fgen("v1 => 3, v2 => -2"));
+
+julia> $(fgen("Dict(v1 => 3, v2 => -2)"));
+""" * footer
+
+"""
+    ΔNout{T}(args...;fallback::F)
+
+Return a `ΔNout{T, V, F}` created from `args` where `args` can be either a `Dict` or arguments used to create a `Dict`
+(e.g. a set of `Pair`s or a generator).
+
+Also helps creating a `Dict{V, Int}` from e.g. a `Dict{Any, Any}` as a convenience.
+
+$(generic_Δnout_docstring_examples(args -> "ΔNout{Exact}($args; fallback=ThrowΔSizeFailError()"))
+"""
 ΔNout{T}(v::AbstractVertex, Δ::Integer; fallback) where T = ΔNout{T}(v=>Int(Δ); fallback)
 ΔNout{T}(args...; fallback) where T = ΔNout{T}(Dict(args...); fallback)
-function ΔNout{T}(Δs::AbstractDict; fallback) where T 
-    @assert all(v -> v isa Int, values(Δs)) string("All values must be of type Int. Got ", values(Δs))
-    return ΔNout{T}(Dict(k=>v for (k,v) in Δs); fallback)
-end
-ΔNout{T}(Δs::AbstractDict{V, Int}; fallback) where {T,V} = ΔNout{T,V,typeof(fallback)}(Dict(Δs), fallback)
-fallback(s::ΔNout) = s.fallback
+ΔNout{T}(Δs::AbstractDict; fallback) where T = ΔNout{T}(Dict(k=>Int(v) for (k,v) in Δs); fallback)
+ΔNout{T}(Δs::AbstractDict{V, Int}; fallback::F) where {T,V,F} = ΔNout{T,V,F}(Dict(Δs), fallback)
+
+"""
+    ΔNoutExact(args...; fallback)
+
+Return a ΔNout{Exact} with `fallback` set to give a warning and then try again with `ΔNoutRelaxed(args...)`.
+
+Accepts either a `Dict` or arguments used to create a `Dict` (e.g. a set of `Pair`s or a generator).
+
+$(generic_Δnout_docstring_examples("ΔNoutExact"))
+"""
+ΔNoutExact(args...; fallback=default_noutfallback(ΔNoutRelaxed,"nout", args)) = ΔNout{Exact}(args...;fallback)
+
+"""
+    ΔNoutRelaxed(args...;fallback)
+
+Return a ΔNout{Relaxed} with `fallback` set to `ThrowΔSizeFailError`.
+
+Accepts either a `Dict` or arguments used to create a `Dict` (e.g. a set of `Pair`s or a generator).
+
+$(generic_Δnout_docstring_examples("ΔNoutRelaxed"))
+"""
+ΔNoutRelaxed(args...;fallback=default_noutfallback("nout", args)) = ΔNout{Relaxed}(args...;fallback)
 
 Δnout_err_info(v, Δ::Union{<:Maybe{Int}, <:Pair}...) = "$(nameorrepr(v)) by $(join(first(Δ), ", ", " and "))"
 Δnout_err_info(v, Δ::Tuple) = Δnout_err_info(v, Δ...)
@@ -118,42 +174,57 @@ fallback(s::ΔNout) = s.fallback
 Δnout_err_info(d::AbstractDict) = join((Δnout_err_info(k, v) for (k,v) in d), ", ", " and ")
 
 function default_noutfallback(nextfallback, dirstr, args) 
-    LogΔSizeExec(v -> string("Could not change ", dirstr, " of ", Δnout_err_info(args...), "! Relaxing constraints..."), Logging.Warn, nextfallback(args...))
+    msgfun = v -> string("Could not change ", dirstr, " of ", Δnout_err_info(args...), "! Relaxing constraints...")
+    return LogΔSizeExec(msgfun, Logging.Warn, nextfallback(args...))
 end
 
-default_noutfallback(dirstr, args) = ThrowΔSizeFailError(string("Could not change ", dirstr, " of ", Δnout_err_info(args...), "!!"))
-
+default_noutfallback(dirstr, args) = ThrowΔSizeFailError(() -> string("Could not change ", dirstr, " of ", Δnout_err_info(args...), "!!"))
 
 """
-    ΔNin{T} <: AbstractJuMPΔSizeStrategy
-    ΔNin{T}(vertex::AbstractVertex, Δs::Vector{Maybe{Int}}, fallback::AbstractJuMPΔSizeStrategy)
-    ΔNinExact(vertex::AbstractVertex, Δs::Vector{Maybe{Int}})
-    ΔNinRelaxed(vertex::AbstractVertex, Δs::Vector{Maybe{Int}})
+    ΔNinExact(args...; fallback)
 
-Strategy for changing nin of `vertex` by `Δs`, i.e new size is `nin(vertex) .+ Δs`. Note that `Δs` must have the same number of elements as `nin(vertex)`.
+Return a `ΔNout{Exact}` configured to set `nout` of the inputs to vertices in `args` to the given sizes.
 
-Use `missing` to indicate "no change required" as 0 will be interpreted as "must not change".
+For vertices with more than one input, the size change must be expressed as a tuple with one element per input. 
+Use `missing` to indicate that no special treatment is needed for an input.
 
-If `T == Exact`, size change will be added as a constraint to the model which means that the operation will fail if it is not possible to change `nin(vertex)` by exactly `Δs`.
+Accepts either a `Dict` or arguments used to create a `Dict` (e.g. a set of `Pair`s or a generator).
 
-If `T == Relaxed`, size change will be added as an objective to the model which means that `nin(vertex)` might not change by exactly `Δs`. In addition, a constraint that `nin(vertex)` must change is also added.
+By default, `fallback` is set to give a warning and then try again with `ΔNinRelaxed(args...)`.
 
-If the operation fails, it will be retried with the `fallback` strategy (default `ΔNin{Relaxed}` if `T==Exact` and `ThrowΔSizeFailError` if `T==Relaxed`).
+$(generic_Δnin_docstring_examples("ΔNinExact"))
 """
-ΔNinExact(args...; fallback=default_noutfallback(ΔNinRelaxed, "nin", args)) = ΔNin(Exact(), args...; fallback)
-ΔNinRelaxed(args...; fallback=default_noutfallback("nout", args)) = ΔNin(Relaxed(), args...;fallback)
-ΔNin(::T, args...; fallback) where T<:Union{Relaxed, Exact} = ΔNout{T}(Δnin2Δnout(args...); fallback)
+ΔNinExact(args...; fallback=default_noutfallback(ΔNinRelaxed, "nin", args)) = ΔNout{Exact}(Δnin2Δnout(args...); fallback)
+
+"""
+    ΔNinRelaxed(args...; fallback)
+
+Same as [`ΔNinExact`](@ref) except `Exact` is replaced by `Relaxed` and `fallback` set to `ThrowΔSizeFailError` by default.
+
+$(generic_Δnin_docstring_examples("ΔNinRelaxed"))
+"""
+ΔNinRelaxed(args...; fallback=default_noutfallback("nout", args)) = ΔNout{Relaxed}(Δnin2Δnout(args...); fallback)
 
 Δnin2Δnout(d::AbstractDict) = reduce(merge!, (Δnin2Δnout(k,v) for (k,v) in d); init=Dict())
 Δnin2Δnout(ps::Pair{<:AbstractVertex}...) = mapreduce(p -> Δnin2Δnout(p...), merge!, ps; init=Dict()) 
 Δnin2Δnout(v::AbstractVertex, Δs) = Δnin2Δnout(v, tuple(Δs))
 Δnin2Δnout(v::AbstractVertex, Δs::Pair{<:Tuple, <:Union{Relaxed, Exact}}) = Dict(k => (v => last(Δs)) for (k,v) in Δnin2Δnout(v, first(Δs))) 
 function Δnin2Δnout(v::AbstractVertex, Δs::Tuple)
-    @assert length(Δs) == length(inputs(v)) "Must supply same number of Δs as v has inputs! Got $Δs for $(nameorrepr(v))."
+    @assert length(Δs) == length(inputs(v)) "Must supply same number of Δs as v has inputs! Got $Δs for $(nameorrepr(v)). Tip: Use missing to indicate that no special requirement is used for an input"
     inds = findall(!ismissing, Δs)
     return Dict(inputs(v)[i] => Δs[i] for i in inds)
 end
 
+"""
+    ΔNoutMix{VE, VR, F} <: AbstractJuMPΔSizeStrategy
+    ΔNoutMix(exact::ΔNout{Exact, VE, F}, relax::ΔNout{Relaxed, VR, F}, fallback::F)
+
+Strategy for changing nout of vertices.
+
+Applies both `exact` and `relax`, thereby allowing for having both hard and relaxed size constraints for different vertices.
+
+Can be conveniently created by [`ΔNout`](@ref) or [`ΔNin`](@ref). 
+"""
 struct ΔNoutMix{VE, VR, F} <: AbstractJuMPΔSizeStrategy
     exact::ΔNout{Exact, VE, F}
     relax::ΔNout{Relaxed, VR, F}
@@ -161,17 +232,47 @@ struct ΔNoutMix{VE, VR, F} <: AbstractJuMPΔSizeStrategy
 end
 fallback(s::ΔNoutMix) = s.fallback
 
-relaxed(i::Int) = i => Relaxed()
-relaxed(t::Tuple{Vararg{Int}}) = t => Relaxed()
+"""
+    relaxed(Δ)
 
+Return `Δ => Relaxed()` which indicates that `Δ` shall be relaxed when changing size.
+
+See [`Δnout`](@ref) and [`Δnin`](@ref).
+"""
+relaxed(Δ::Integer) = Δ => Relaxed()
+relaxed(Δs::Tuple{Vararg{<:Maybe{Int}}}) = Δs => Relaxed()
+
+"""
+    ΔNout(args...)
+
+Splits `args` into relaxed and exact size changes and creates the appropriate strategy (one of `ΔNout{Exact}, ΔNout{Relaxed} or ΔNoutMix`).
+
+$(generic_Δnout_docstring_examples("ΔNout"; footer=""))
+julia> ΔNout(v1, relaxed(2));
+
+julia> ΔNout(v1 => relaxed(2), v2 => -1)
+``` 
+"""
+function ΔNout(args...) 
+    exact, relaxed = split_exact_relaxed(args)
+    return ΔNout(exact, relaxed)
+end
+
+"""
+    ΔNin(args...)
+
+Splits `args` into relaxed and exact size changes and creates the appropriate strategy (one of `ΔNout{Exact}, ΔNout{Relaxed} or ΔNoutMix`).
+
+$(generic_Δnin_docstring_examples("ΔNin"; footer=""))
+julia> ΔNin(v1, relaxed(3), missing, 2);
+
+julia> ΔNin(v1 => (relaxed(3), missing, 2), v2 => relaxed(-2, 0))
+``` 
+"""
 function ΔNin(args...) 
     exact,relaxed = split_exact_relaxed(Δnin2Δnout(args...)) # To support mixed cases, e.g. Δnin(v => (2, relaxed(4)))
     fallback = default_noutfallback((args...) -> ΔNoutRelaxed(merge(exact, relaxed); fallback=default_noutfallback("nin", args)), "nin", args)
     return ΔNout(exact, relaxed, fallback)
-end
-function ΔNout(args...) 
-    exact, relaxed = split_exact_relaxed(args)
-    return ΔNout(exact, relaxed)
 end
 
 function ΔNout(exact::Dict, relaxed::Dict, fb = default_noutfallback(ΔNoutRelaxed, "nout", merge(exact,relaxed)))
@@ -202,6 +303,8 @@ end
     AlignNinToNout(vstrat=DefaultJuMPΔSizeStrategy())
 
 Adds variables and constraints for `nin(vi) == nout.(inputs(vi))`.
+
+Generally useful when doing structural changes such are removing/adding vertices or edges.
 
 If it fails, the operation will be retried with the `fallback` strategy (default `ThrowΔSizeFailError`).
 """
@@ -260,11 +363,25 @@ SelectDirection() = SelectDirection(DefaultJuMPΔSizeStrategy())
     TruncateInIndsToValid()
     TruncateInIndsToValid(s::S)
 
-Ensures that all selected input indices are within range of existing input indices after applying `s` (default `DefaultJuMPSelectionStrategy`).
+Ensures that all selected input indices are within range of existing input indices after applying `s` 
+(default `DefaultJuMPSelectionStrategy`).
 
-Not needed in normal cases, but certain structural mutations (e.g create_edge!) may cause this to happen due to how constraints are (not) created when original sizes do not align in conjunction with how result of selection is interpreted.
+Not needed in normal cases, but certain structural mutations (e.g create_edge!) may cause this to happen 
+due to how constraints are (not) created when original sizes do not align in conjunction with how result of 
+selection is interpreted.
 
-While this may be considered a flaw in the output selection procedure, it is rare enough so that in most cases when it happens it is the result of a user error or lower level bug. Therefore this strategy is left optional to be used only in cases when mismatches are expected.
+An example of when it is needed is when adding an edge from vertex `vi` to an invariant vertex `vo` where 
+`nout(vi) > nout(vo)`.In this case it is expected that the result of the optimization is that the indices 
+`1:nout(vi)` of `vi` shall be kept. However, this will propagate to `vo` which will be instructed to keep 
+indices it does not have. With this strategy, all indices which are larger than `nout(vo)` will be replaced
+by `-1` (which indicates that new parameters shall be created) 
+
+While this may be considered a flaw in the output selection procedure, it is rare enough so that in most cases 
+when it happens it is the result of a user error or lower level bug. Therefore this strategy is left optional 
+to be used only in cases when mismatches are expected.
+
+Note that in most normal cases, this has no effect since vertices capable of getting new input edges generally 
+don't have parameters.
 """
 struct TruncateInIndsToValid{S} <: AbstractΔSizeStrategy
     strategy::S
