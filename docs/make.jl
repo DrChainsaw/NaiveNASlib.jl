8efcad4e76a9b29c33e5fef034305d42b31ac25a
DrChainsaw <Christian.kyril.skarby@gmail.com>
2025-08-30 14:15:25 +0200

    Bump Julia version to 1.11 Add COLUMNS handling in make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,71 +2,83 @@ using Documenter, Literate, NaiveNASlib, NaiveNASlib.Advanced, NaiveNASlib.Exten
 
 NaiveNASlib.GRAPHSUMMARY_USE_HIGHLIGHTS[] = false
 
+# This is to avoid truncation of outputs in doostrings, primarily for graphsummary
+defaultcols = haskey(ENV, "COLUMNS") ? ENV["COLUMNS"] : nothing
+ENV["COLUMNS"] = 1000
 const nndir = joinpath(dirname(pathof(NaiveNASlib)), "..")
 
-function literate_example(sourcefile; rootdir=nndir, sourcedir = "test/examples", destdir="docs/src/examples", kwargs...)
-    fullpath = Literate.markdown(joinpath(rootdir, sourcedir, sourcefile), joinpath(rootdir, destdir); flavor=Literate.DocumenterFlavor(), mdstrings=true, kwargs...)
-    dirs = splitpath(fullpath)
-    srcind = findfirst(==("src"), dirs)
-    joinpath(dirs[srcind+1:end]...)
-end
+try
 
-quicktutorial = literate_example("quicktutorial.jl")
-advancedtutorial = literate_example("advancedtutorial.jl"; codefence="````julia" => "````")
+    function literate_example(sourcefile; rootdir=nndir, sourcedir = "test/examples", destdir="docs/src/examples", kwargs...)
+        fullpath = Literate.markdown(joinpath(rootdir, sourcedir, sourcefile), joinpath(rootdir, destdir); flavor=Literate.DocumenterFlavor(), mdstrings=true, kwargs...)
+        dirs = splitpath(fullpath)
+        srcind = findfirst(==("src"), dirs)
+        joinpath(dirs[srcind+1:end]...)
+    end
 
-makedocs(   sitename="NaiveNASlib",
-            root = joinpath(nndir, "docs"), 
-            format = Documenter.HTML(
-                prettyurls = get(ENV, "CI", nothing) == "true"
-            ),
-            pages = [
-                "index.md",
-                quicktutorial,
-                advancedtutorial,
-                "terminology.md",
-                "API Reference" => [
-                    "reference/simple/createvertex.md",
-                    "reference/simple/graph.md",
-                    "reference/simple/queryvertex.md",
-                    "reference/simple/mutatevertex.md",
-                    "Advanced" => [
-                        "reference/advanced/graphquery.md",
-                        "reference/advanced/size.md",
-                        "reference/advanced/structure.md",
-                        "reference/advanced/traits.md",
-                        "reference/advanced/infixconf.md",
+    quicktutorial = literate_example("quicktutorial.jl")
+    advancedtutorial = literate_example("advancedtutorial.jl"; codefence="````julia" => "````")
+
+    makedocs(   sitename="NaiveNASlib",
+                root = joinpath(nndir, "docs"), 
+                format = Documenter.HTML(
+                    prettyurls = get(ENV, "CI", nothing) == "true"
+                ),
+                pages = [
+                    "index.md",
+                    quicktutorial,
+                    advancedtutorial,
+                    "terminology.md",
+                    "API Reference" => [
+                        "reference/simple/createvertex.md",
+                        "reference/simple/graph.md",
+                        "reference/simple/queryvertex.md",
+                        "reference/simple/mutatevertex.md",
+                        "Advanced" => [
+                            "reference/advanced/graphquery.md",
+                            "reference/advanced/size.md",
+                            "reference/advanced/structure.md",
+                            "reference/advanced/traits.md",
+                            "reference/advanced/infixconf.md",
+                        ],
+                        "Extend" => [
+                            "reference/extend/vertices.md",
+                            "reference/extend/strategies.md",
+                            "reference/extend/traits.md",
+                            "reference/extend/misc.md",
+                        ],
                     ],
-                    "Extend" => [
-                        "reference/extend/vertices.md",
-                        "reference/extend/strategies.md",
-                        "reference/extend/traits.md",
-                        "reference/extend/misc.md",
+                    "Internal" => [
+                        "reference/internal/internal.md",
                     ],
                 ],
-                "Internal" => [
-                    "reference/internal/internal.md",
-                ],
-            ],
-            modules = [NaiveNASlib],
-            warnonly=[:missing_docs],
+                modules = [NaiveNASlib],
+                warnonly=[:missing_docs],
+            )
+    function touchfile(filename, rootdir=nndir, destdir="test/examples")
+        filepath = joinpath(rootdir, destdir, filename)
+        isfile(filepath) && return
+        write(filepath, """
+        md\"\"\"
+        # Markdown header
+        \"\"\"
+        """)
+    end
+
+    if get(ENV, "CI", nothing) == "true"
+        deploydocs(
+            repo = "github.com/DrChainsaw/NaiveNASlib.jl.git",
+            push_preview=true
         )
-function touchfile(filename, rootdir=nndir, destdir="test/examples")
-    filepath = joinpath(rootdir, destdir, filename)
-    isfile(filepath) && return
-    write(filepath, """
-    md\"\"\"
-    # Markdown header
-    \"\"\"
-    """)
-end
+    end
 
-if get(ENV, "CI", nothing) == "true"
-    deploydocs(
-        repo = "github.com/DrChainsaw/NaiveNASlib.jl.git",
-        push_preview=true
-    )
+finally
+    NaiveNASlib.GRAPHSUMMARY_USE_HIGHLIGHTS[] = true
+    if isnothing(defaultcols)
+        delete!(ENV, "COLUMNS")
+    else
+        ENV["COLUMNS"] = defaultcols
+    end
 end
-
-NaiveNASlib.GRAPHSUMMARY_USE_HIGHLIGHTS[] = true
 
 nothing # Just so that include("make.jl") does not return anything
